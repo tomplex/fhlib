168fafe972c039668a769364ae755e6419580a63
tom c <carusot42@gmail.com>
2016-10-17 00:34:46 -0400

    change load structure

--- fhlib/fhlib.py
+++ fhlib/fhlib.py
@@ -1,13 +1,20 @@
-from fhlparsers import PlayerParser, ScoreboardParser, TeamParser, StandingsParser
-
+from fhlib.fhlparsers import PlayerParser, ScoreboardParser, TeamParser, StandingsParser
 from fhlib.config import DEFAULT_SEASON
 
 
 class League:
     def __init__(self, league_id, season=DEFAULT_SEASON):
         self.league_id = league_id
-        self.free_agents = PlayerParser(league_id)
-        self.scoreboard = ScoreboardParser(league_id)
-        self.teams = TeamParser(league_id)
-        self.standings = StandingsParser(league_id)
+
+    def __load_freeagents(self):
+        self.free_agents = PlayerParser()
+
+    def __load_scoreboard(self):
+        self.scoreboard = ScoreboardParser()
+
+    def __load_teams(self):
+        self.teams = TeamParser()
+
+    def __load_standings(self):
+        self.standings = StandingsParser()
 
