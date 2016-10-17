d7668bf366746140e001796885b73aef792cf984
tom c <carusot42@gmail.com>
2016-10-17 00:35:53 -0400

    more representative example of what -Parser class will look like.....eventually

--- fhlib/fhlparsers/players.py
+++ fhlib/fhlparsers/players.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
+from fhlib.config import DEFAULT_HTML_PARSER
 
 
 class PlayerParser:
 
-    def __init__(self, league_id):
-        pass
+    def __init__(self, html):
+        self.parsed = BeautifulSoup(html, DEFAULT_HTML_PARSER)
+        
